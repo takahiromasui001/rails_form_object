69248365a1390800cb6728edee3eb4ef626911de
Takahiro Masui <takahiro.masui@luxiar.com>
2021-01-07 18:53:29 +0900

    new/create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :tag_names)
+    params.require(:post_form).permit(:title, :content, :tag_names)
   end
 end
--- app/forms/post_form.rb
+++ app/forms/post_form.rb
@@ -9,6 +9,15 @@ class PostForm
     super(attributes)
   end
 
+  def save
+    ActiveRecord::Base.transaction do
+      tags = tag_names.split(',').map { |name| Tag.find_or_create_by!(name: name) }
+      post.update!(name: title, content: content, tags: tags)
+    end
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+
   private
 
   attr_reader :post
