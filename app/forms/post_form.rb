e249fa3880bffda8a17fadcb1197582297dd6895
Takahiro Masui <takahiro.masui@luxiar.com>
2021-01-07 21:06:29 +0900

    1からFormObject, new/createまで

--- app/forms/post_form.rb
+++ app/forms/post_form.rb
@@ -1,38 +1,22 @@
 class PostForm
-  include ActiveModel::Model
+  def initialize(params = nil)
+    return if params.nil?
 
-  attr_accessor :title, :content, :tag_names
-
-  delegate :persisted?, to: :post
-
-  def initialize(attributes = nil, post: Post.new)
-    @post = post
-    attributes ||= default_attributes
-    super(attributes)
+    @name = params[:title]
+    @content = params[:content]
+    @tag_names = params[:tag_names]
   end
 
   def save
     ActiveRecord::Base.transaction do
-      tags = tag_names.split(',').map { |name| Tag.find_or_create_by!(name: name) }
-      post.update!(name: title, content: content, tags: tags)
+      tags = @tag_names.split(',').map { |tag| Tag.find_or_create_by(name: tag) }
+      Post.create!(name: @name, content: @content, tags: tags)
     end
   rescue ActiveRecord::RecordInvalid
     false
   end
 
   def to_model
-    post
-  end
-
-  private
-
-  attr_reader :post
-
-  def default_attributes
-    {
-      title: post.name,
-      content: post.content,
-      tag_names: post.tags.pluck(:name).join(',')
-    }
+    Post.new
   end
 end
